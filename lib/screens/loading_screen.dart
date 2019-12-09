d05ac148f87476733c7b2cfb9bc3805f331c934d
Wishman <gjwisher@aol.com>
2019-12-09 19:12:11 +0100

    End of L-130 Getting Location Data (Step 1) import geolocator and use it to get gps data on android and ios

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart'; // 1.1
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,20 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // 1.2
+  void getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low); // 1.3
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            // 1.5 - Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
