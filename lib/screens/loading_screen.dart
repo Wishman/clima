6529baa5c0f59928c13ec97a4f24ff81712e539c
Wishman <gjwisher@aol.com>
2019-12-14 13:36:37 +0100

    End of L-136  HTTP Package - getting data from Internet API(Step 6) async /await in loading screen required Future in location.dart! added to getLocation() added getData()

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart'; // 5.5(a)
+import 'package:http/http.dart' as http; // 6.2
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,14 +15,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
+  // async / await needed to make sure data is actually here
   void getLocation() async {
     Location loc = Location(); // 5.5(b)
     await loc.getCurrentLocation(); // 5.5(c)
     print('longitude: ${loc.longitude}, latitude: ${loc.latitude}');
   }
 
+  // 6.3 - use APi version that accepts lat & long
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2'
+        '.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    /* old stuff
     // 4.1(a) & (e)
     String myMargin = 'abc';
     double myMarginAsDouble;
@@ -36,11 +52,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
       //print(e);
       // 4.1 (f) - on error, provide default value
     }
+    */
+    getData(); //6.3
+
     // 4.1(b) & (e)
-    return Scaffold(
-        body: Container(
-      margin: EdgeInsets.all(myMarginAsDouble ?? 30.0), // 4.1(g)
-      color: Colors.red,
-    ));
+    return Scaffold();
   }
 }
